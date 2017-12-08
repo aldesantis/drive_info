95514a9d9ad5392ecb1e7031864a8cc4166331e0
Diogo Dias <e.diogodias@gmail.com>
2017-12-08 11:59:49 +0000

    Added batch request support

--- lib/drive_info/route_time.rb
+++ lib/drive_info/route_time.rb
@@ -5,5 +5,9 @@ module DriveInfo
     def route_time(options)
       adapter.route_time(options)
     end
+
+    def batch_route_time(options)
+      adapter.batch_route_time(options)
+    end
   end
 end
